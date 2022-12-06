564e090032176c93a4bd4ee3490b166fbef581c8
isir10 <isir.malak@moringaschool.com>
2022-12-06 19:32:39 +0300

    GWC-5 <added slider component which enbales my images to slide based on scrolling side>

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import ship0 from "./assets/ship0.jpeg"
 import ship2 from "./assets/ship2.jpg"
 import ship3 from "./assets/ship3.jpg"
 import Hero from './components/Hero';
+import Slider from './components/Slider';
 
 
 function App() {
   return (
     <div className="App">
       <Hero imageSrc={ship1} />
+      <Slider
+        imageSrc={ship2}
+        title={"Dependable Shipping."}
+        subtitle={
+          "At Good Will Cargo, we offer tailored alternatives for the movement…"
+        }
+      />
+      <Slider
+        imageSrc={ship3}
+        title={"Fast Shipping."}
+        subtitle={"Good Will Cargo delivers fast, safe and sound sea cargo solutions.."}
+        flipped={true}
+      />
       
     </div>
   );
