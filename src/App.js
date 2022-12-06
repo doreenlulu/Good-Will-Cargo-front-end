5c8e64234b48ccf62e1939ffa641b01c16ff906a
isir10 <isir.malak@moringaschool.com>
2022-12-06 18:42:50 +0300

    GWC-5 <created my first image component>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import ship1 from "./assets/ship1.jpg"
+import ship0 from "./assets/ship0.jpeg"
+import ship2 from "./assets/ship2.jpg"
+import ship3 from "./assets/ship3.jpg"
+import Hero from './components/Hero';
+
 
 function App() {
   return (
     <div className="App">
+      <Hero imageSrc={ship1} />
       
     </div>
   );
