41d5ca768016b3a4c7a3061c405afdb7ca894cbd
Doreen <doreenlulu97@gmail.com>
2022-12-08 00:35:36 +0300

    Edited some routes

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Product.css";
-import ship2 from "./assets/ship2.jpg"
+// import ship2 from "./assets/ship2.jpg"
 
 function Product () {
     return (
@@ -8,50 +8,55 @@ function Product () {
           <h1>PRODUCTS AVAILABLE</h1>
           <div className='product__container'>
           <div className='product__wrapper'>
+            
           <ul className='product__list'>
             <Product
-              imageSrc={ship2}
+              src='/home/doreen/Good-Will-Cargo-front-end/src/assets/ship1.jpg'
               text='product.'
               label='product'
               path='/products'
             />
             <Product
-              imageSrc={ship2}
+              src='/home/doreen/Good-Will-Cargo-front-end/src/assets/ship1.jpg'
               text='product'
               label='product'
               path='/products'
             />
           </ul>
+
           <ul className='product__list'>
             <Product
-              imageSrc={ship2}
+              src='/home/doreen/Good-Will-Cargo-front-end/src/assets/ship1.jpg'
               text='product'
               label='product'
               path='/products'
             />
             <Product
-              imageSrc={ship2}
+              src='/home/doreen/Good-Will-Cargo-front-end/src/assets/ship1.jpg'
               text='product'
               label='product'
               path='/products'
             />
           </ul>
+
           <ul className='product__list'>
             <Product
-              imageSrc={ship2}
+              src='/home/doreen/Good-Will-Cargo-front-end/src/assets/ship1.jpg'
               text='product'
               label='product'
               path='/products'
             />
             <Product
-              imageSrc={ship2}
+              src='/home/doreen/Good-Will-Cargo-front-end/src/assets/ship1.jpg'
               text='product'
               label='product'
               path='/products'
             />
           </ul>
         </div>
+
           <h2>Products available at your disposal</h2>
+
       </div>
     </div>
     );
