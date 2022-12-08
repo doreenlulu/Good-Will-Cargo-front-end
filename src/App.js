72988e2a5e636dbd3b6e9f84651157741f6acfd6
Doreen <doreenlulu97@gmail.com>
2022-12-08 17:00:20 +0300

    testing git

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
-import './App.css';
-import ship1 from "./assets/ship1.jpg"
-import ship2 from "./assets/ship2.jpg"
-import ship3 from "./assets/ship3.jpg"
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Slider from './components/Slider';
-import Register from './form/Register'; 
-import {Route, Routes} from "react-router-dom";
-
+import './styling/App.css';
+import Nav from './components/Nav';
+import { useLoggedInContext } from './context/LoggedIn';
+import Body from './components/Body';
+import { useEffect } from 'react';
 
 function App() {
-  
-  return (
-    <div className="App">
-       <Navbar />
-     
-      <Routes>
-        <Route path='/Register' element={<Register/>}/>
-        <Route path='/' element={<> <Hero imageSrc={ship1} />
-      <Slider
-        imageSrc={ship2}
-        title={"Dependable Shipping."}
-        subtitle={
-          "At Good Will Cargo, we offer tailored alternatives for the movement…"
-        }
-      />
-      <Slider
-        imageSrc={ship3}
-        title={"Fast Shipping."}
-        subtitle={"Good Will Cargo delivers fast, safe and sound sea cargo solutions.."}
-        flipped={true}
-      /></>}/>
-      </Routes>
-    </div>
-  );
-}
+  const { setLoggedIn } = useLoggedInContext();
+  useEffect(()=>{
+    fetch("https://good-will-cargo-spark-production.up.railway.app/me",{
+      headers: {"Authorization": `Bearer ${localStorage.getItem("jwt")}`}
+    })
+    .then(r=>r.json())
+    .then(loggedUser=>{
+      setLoggedIn((prevData)=>
+    ({...prevData, user:{...prevData.user, user: loggedUser}}))
+    fetch("https://good-will-cargo-spark-production.up.railway.app/user_cargos",{
+                    headers: {"Authorization": `Bearer ${localStorage.getItem("jwt")}`}
+            })
+            .then(r=>r.json())
+            .then(cargos=>{
+                setLoggedIn(prevData=>({user:{...prevData.user,cargos: cargos}}))
+            })
+    })
+  },[])
+//   return (
+//     <>
+//        <Nav/>
+//       <Body/>
+//     </>
+//   );
+// }
 
 export default App;
